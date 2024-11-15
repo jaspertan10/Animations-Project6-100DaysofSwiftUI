ff86ef153075e2ed477a0d0091f20e4a1b40d76d
Jasper Tan <jatan@Jaspers-Mac-mini.local>
2024-11-15 11:49:28 -0800

    animation sandbox view testing

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -20,5 +20,77 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    //ContentView()
+    SandBoxView()
+}
+
+struct SandBoxView: View {
+    
+    @State private var animationAmount = 1.0
+    
+    
+    var body: some View {
+        
+
+//        Button("Tap Me") {
+//            animationAmount += 1
+//        }
+//        .padding(50)
+//        .background(.red)
+//        .foregroundStyle(.white)
+//        .clipShape(.circle)
+//        .scaleEffect(animationAmount)
+//        .blur(radius: (animationAmount - 1))
+//        .animation(
+//            .easeInOut(duration: 1)
+//                .repeatCount(3, autoreverses: false),
+//            value: animationAmount
+//        )
+        
+    
+            print(animationAmount)
+
+            return VStack {
+            /* Button with overlay circle*/
+            Button("Tap Me") {
+                // animationAmount += 1
+            }
+            .padding(50)
+            .background(.red)
+            .foregroundStyle(.white)
+            .clipShape(.circle)
+            .overlay(
+                Circle()
+                    .stroke(.red)
+                    .scaleEffect(animationAmount)
+                    .opacity(2 - animationAmount)
+                    .animation(
+                        .easeInOut(duration: 1)
+                        .repeatForever(autoreverses: false),
+                        value: animationAmount
+                    )
+            )
+            .onAppear {
+                animationAmount = 2
+            }
+            
+            Spacer()
+            
+            
+            VStack {
+                Stepper("Scale amount", value: $animationAmount.animation(), in: 1...10)
+                
+                Spacer()
+                
+                Button("Tap Me") {
+                    animationAmount += 1
+                }
+                .padding(40)
+                .background(.red)
+                .foregroundStyle(.white)
+                .clipShape(.circle)
+                .scaleEffect(animationAmount)
+            }
+        }
+    }
 }
